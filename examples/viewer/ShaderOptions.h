446a12ac083b87fb9c8319fe172f21c2da9b21d9
Jesse Y <jesse.y@gmail.com>
2019-03-14 22:09:09 -0700

    Update copyright for 2019

--- examples/viewer/ShaderOptions.h
+++ examples/viewer/ShaderOptions.h
@@ -1,5 +1,5 @@
 // ------------------------------------------------------------
-// Copyright(c) 2018 Jesse Yurkovich
+// Copyright(c) 2019 Jesse Yurkovich
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 // See the LICENSE file in the repo root for full license information.
 // ------------------------------------------------------------
